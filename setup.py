ddea03d0561f744c96a2946d4f4ed65675242143
Janusz Prusaczyk (JJPR) <jjpr@demant.com>
2020-08-13 19:56:19 +0200

    Initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'epubproject',
+    version = '0.0.1',
+    description = 'Build EPub files from data',
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    install_requires = [
+       "Jinja2",
+       "PyYAML",
+       "Markdown"
+    ]
+)
